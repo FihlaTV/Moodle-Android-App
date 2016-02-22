5658f30af3a5a5e8ac524e71db14040d47ce4a92
Kapil Kumar <masterkapilkumar@gmail.com>
2016-02-22 17:30:29 +0530

    Added the course details page

---
+++ app/src/main/java/com/cyberthieves/trialapp/CourseDetails.java
@@ -0,0 +1,110 @@
+package com.cyberthieves.trialapp;
+
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.NavUtils;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cyberthieves.trialapp.fragments.*;
+
+public class CourseDetails extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignments);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        Overview overview = new Overview();
+        overview.course_code=getIntent().getStringExtra("course_code");
+        overview.course_name=getIntent().getStringExtra("course_name");
+        overview.course_description=getIntent().getStringExtra("course_description");
+        Assignments assignments = new Assignments();
+        assignments.course_code=getIntent().getStringExtra("course_code");
+        Resources resources = new Resources();
+        resources.course_code=getIntent().getStringExtra("course_code");
+        Threads threads = new Threads();
+        threads.course_code = getIntent().getStringExtra("course_code");
+        Grades grades = new Grades();
+        grades.course_code = getIntent().getStringExtra("course_code");
+        adapter.addFrag(overview, "Overview");
+        adapter.addFrag(assignments, "Assignments");
+        adapter.addFrag(resources, "Resources");
+        adapter.addFrag(threads, "Threads");
+        adapter.addFrag(grades, "Grades");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFrag(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                super.onBackPressed();
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+}
