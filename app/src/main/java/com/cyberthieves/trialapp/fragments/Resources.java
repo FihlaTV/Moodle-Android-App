37e30509bedeac3b742bdbbf57d074781ddc059e
Sourav Das <bdas1547@gmail.com>
2016-02-23 18:19:17 +0530

    Added resources and threads page

---
+++ app/src/main/java/com/cyberthieves/trialapp/fragments/Resources.java
@@ -0,0 +1,32 @@
+package com.cyberthieves.trialapp.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cyberthieves.trialapp.R;
+
+
+public class Resources extends Fragment{
+
+    public String course_code;
+
+    public Resources() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_resources, container, false);
+    }
+
+}
